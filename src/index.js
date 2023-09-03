d6646b2257e49fd7f49f3f51330ecfdce1a00dfb
unknown <lstradiot15@gmail.com>
2023-09-03 12:03:47 -0300

    Working on hamburguer menu

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Views/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <Header />
+    <Home />
+    <Footer />
+  </React.StrictMode>
+);
