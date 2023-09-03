7402987e25c9c8efa4f495d732b7c4ec3781af8d
unknown <lstradiot15@gmail.com>
2023-09-03 16:50:51 -0300

    Added footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import styles from './footer.module.css';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p>© Luciano Stradiot</p>
+        <div className={styles.subContainer}>
+          <a
+            href="https://www.linkedin.com/in/luciano-stradiot-343a98142/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="assets/linkedin.png" className={styles.images} />
+          </a>
+          <a href="https://github.com/LucianoStradiot" target="_blank" rel="noreferrer">
+            <img src="assets/github.png" className={styles.images} />
+          </a>
+          <a href="https://www.instagram.com/luchostradiot/" target="_blank" rel="noreferrer">
+            <img src="assets/instagram.png" className={styles.images} />
+          </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=100094383460961"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="assets/facebook.png" className={styles.images} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
