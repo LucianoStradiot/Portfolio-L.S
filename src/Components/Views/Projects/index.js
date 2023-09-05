c36775260edf91d030cd268dd2bcb9b239480355
unknown <lstradiot15@gmail.com>
2023-09-05 17:02:36 -0300

    Added projects view

--- src/Components/Views/Projects/index.js
+++ src/Components/Views/Projects/index.js
@@ -4,10 +4,48 @@ import HamburguerMenu from '../../Shared/HamburguerMenu';
 
 function Projects() {
   return (
-    <div className={styles.container}>
-      <h2>Projects</h2>
-      <HamburguerMenu />
-    </div>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h2>Projects</h2>
+        <HamburguerMenu />
+      </div>
+      <section>
+        <div className={styles.sectionProjects}>
+          <img
+            src="assets/megarocket.png"
+            alt="megarocket-app"
+            className={styles.projectsImages1}
+          />
+          <p className={styles.boxParagraph1}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, excepturi. Dolorem
+            tempore cumque nihil laudantium pariatur iure facilis eius accusamus quidem molestiae
+            ea, veritatis repellendus modi? Illo delectus vero consequatur?
+          </p>
+          <a href="https://mega-rocket-app.vercel.app/" target="_blank" rel="noreferrer">
+            View the project
+          </a>
+        </div>
+        <div className={styles.sectionProjects}>
+          <img
+            src="assets/nutritionistValen.jpg"
+            alt="nutritionist-valentina"
+            className={styles.projectsImages2}
+          />
+          <p className={styles.boxParagraph2}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, excepturi. Dolorem
+            tempore cumque nihil laudantium pariatur iure facilis eius accusamus quidem molestiae
+            ea, veritatis repellendus modi? Illo delectus vero consequatur?
+          </p>
+          <a
+            href="https://nutricionista-valentina-lansellota.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            View the project
+          </a>
+        </div>
+      </section>
+    </main>
   );
 }
 
