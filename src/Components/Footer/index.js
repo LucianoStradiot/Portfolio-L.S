d7eb07a1e53f490060de11e95aaddff5069a5b20
unknown <lstradiot15@gmail.com>
2024-03-05 11:37:21 -0300

    Remove music modal

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,56 +1,14 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React from 'react';
 import styles from './footer.module.css';
-import Modal from '../Shared/Modal';
 import WspButton from '../Shared/WspButton';
 import { useDarkMode } from '../Shared/DarkBtn/darkModeContext';
 
 const Footer = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const audioRef = useRef();
-  const [isOpen, setIsOpen] = useState(false);
   const { isDarkMode } = useDarkMode();
-  const [responseModal, setResponseModal] = useState({
-    title: '',
-    description: '',
-    isConfirm: false
-  });
-
-  const openModalConfirm = () => {
-    setResponseModal({
-      title: 'Welcome!',
-      description: 'Do you want to listen some music?',
-      isConfirm: true
-    });
-    setIsOpen(true);
-  };
-
-  const togglePlay = () => {
-    const audioElement = audioRef.current;
-
-    if (audioElement.paused) {
-      audioElement.play();
-    } else {
-      audioElement.pause();
-    }
-
-    setIsPlaying(!isPlaying);
-  };
-
-  useEffect(() => {
-    openModalConfirm();
-  }, []);
 
   return (
     <footer className={`${isDarkMode ? styles.darkFooter : styles.footer}`}>
       <WspButton />
-      <Modal
-        title={responseModal.title}
-        desc={responseModal.description}
-        isOpen={isOpen}
-        confirmModal={responseModal.isConfirm}
-        handleClose={() => setIsOpen(!isOpen)}
-        activeFunction={() => togglePlay(setIsOpen(!isOpen))}
-      />
       <div className={styles.container}>
         <p className={`${isDarkMode ? styles.darkCopyright : styles.copyright}`}>
           © Luciano Stradiot 2023
@@ -76,25 +34,6 @@ const Footer = () => {
           >
             <img src="assets/facebook.png" className={styles.images} alt="facebook-icon" />
           </a>
-          <span onClick={togglePlay}>
-            {isPlaying ? (
-              <img
-                src="assets/pause.svg"
-                className={`${isDarkMode ? styles.darkPause : styles.pause} ${styles.images}`}
-                alt="pause"
-              />
-            ) : (
-              <img
-                src="assets/play.svg"
-                className={`${isDarkMode ? styles.darkPlay : styles.play} ${styles.images}`}
-                alt="play"
-              />
-            )}
-            <audio ref={audioRef} onLoadedData={() => setIsPlaying(false)} loop>
-              <source src="/synthwave.mp3" type="audio/mp3" />
-              Tu navegador no soporta el elemento de audio.
-            </audio>
-          </span>
         </div>
       </div>
     </footer>
--- src/Components/Shared/Modal/index.js
+++ src/Components/Shared/Modal/index.js
@@ -2,38 +2,21 @@ import React from 'react';
 import styles from './modal.module.css';
 import { useDarkMode } from '../DarkBtn/darkModeContext';
 
-const Modal = ({ title, desc, isOpen, handleClose, confirmModal, activeFunction }) => {
+const Modal = ({ title, desc, isOpen, handleClose }) => {
   const { isDarkMode } = useDarkMode();
 
   return isOpen ? (
-    confirmModal ? (
-      <div className={styles.modal}>
-        <div className={styles.modalContainer}>
-          <h3>{title}</h3>
-          <p>{desc}</p>
-          <div className={styles.btnsContainer}>
-            <button className={`${styles.buttonConfirm} ${styles.button}`} onClick={activeFunction}>
-              Yes🎧!
-            </button>
-            <button className={`${styles.buttonCancel} ${styles.button}`} onClick={handleClose}>
-              No &#129323;
-            </button>
-          </div>
+    <div className={styles.modal}>
+      <div className={`${isDarkMode ? styles.darkModalContainer : styles.modalContainer}`}>
+        <h3>{title}</h3>
+        <p>{desc}</p>
+        <div className={styles.btnContainer}>
+          <button className={`${styles.buttonConfirm} ${styles.button}`} onClick={handleClose}>
+            Confirm
+          </button>
         </div>
       </div>
-    ) : (
-      <div className={styles.modal}>
-        <div className={`${isDarkMode ? styles.darkModalContainer : styles.modalContainer}`}>
-          <h3>{title}</h3>
-          <p>{desc}</p>
-          <div className={styles.btnContainer}>
-            <button className={`${styles.buttonConfirm} ${styles.button}`} onClick={handleClose}>
-              Confirm
-            </button>
-          </div>
-        </div>
-      </div>
-    )
+    </div>
   ) : null;
 };
 
